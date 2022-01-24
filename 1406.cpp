74ceeb177d43516f050e13d5471ceea1a1b58002
snwox <seonu64@gmail.com>
2022-01-24 23:11:50 +0900

    back up to server

---
+++ 1406.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+    list<int> l;
+    list<int>::iterator it;
+    string s;
+    cin >> s;
+    for(auto i:s){
+        cout << i << endl;
+    }
+}
